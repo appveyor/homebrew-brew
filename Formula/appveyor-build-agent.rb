7834a9988da22469d7a259a07022c40b722a3b18
Feodor Fitsner <feodor@appveyor.com>
2020-05-14 10:25:42 -0700

    Update appveyor-build-agent.rb

--- Formula/appveyor-build-agent.rb
+++ Formula/appveyor-build-agent.rb
@@ -1,9 +1,9 @@
 class AppveyorBuildAgent < Formula
   desc "AppVeyor Build Agent - runs AppVeyor build on your server."
   homepage "https://www.appveyor.com"
-  url "https://appveyordownloads.blob.core.windows.net/appveyor/7.0.2653/appveyor-build-agent-7.0.2653-macos-x64.tar.gz"
-  version "7.0.2653"
-  sha256 "dce2e5ac01632bdcfd6be777d99aa7ff5c1e1b9b7f762e9a4d693d9c53fdac8a"
+  url "https://appveyordownloads.blob.core.windows.net/appveyor/7.0.2881/appveyor-build-agent-7.0.2881-macos-x64.tar.gz"
+  version "7.0.2881"
+  sha256 "f4a0baf69aab0905ebdceb0d55068ca5dfbcf10fbd30520a0494c2850e4e73bc"
 
   def install
     # tune config file
